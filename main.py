684f3528582619bbab8efadf783140a6e813a802
Hannah Macdonell <hannah.macdonell1@gmail.com>
2020-09-30 18:01:22 -0300

    Outline ML Q's

---
+++ main.py
@@ -0,0 +1,21 @@
+'''
+
+ML A1
+
+-- Question 2 --
+Describe the dataset you have collected:
+    - total number of samples,
+    - total number of measurements,
+    - brief description of the measurements included,
+    - nature of the group of interest and what differentiates it from the other samples,
+    - sample counts for your group of interest and sample count for the group not of interest.
+
+• Write a program that analyzes each measurement individually.
+• For each measurement, compute the area under the receiver operating characteristic curve (AUC).
+• Provide a printout of the 10 leading measurements (highest AUC), making it clear what those measurements
+    represent in your dataset (these are the measurements with the most obvious potential to inform prediction in any given machine
+    learning algorithm).
+• Provide a printout of this code.
+
+'''
+
